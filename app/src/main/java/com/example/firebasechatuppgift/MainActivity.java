04166353a42527ce8373b4d78f561997a5194688
Avacade2k <57358089+Avacade2k@users.noreply.github.com>
2020-04-30 15:52:43 +0200

    chat list, chat database, welcome message

--- app/src/main/java/com/example/firebasechatuppgift/MainActivity.java
+++ app/src/main/java/com/example/firebasechatuppgift/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.firebasechatuppgift.Model.User;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,26 +21,66 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
-    String username;
+    TextView username;
+    ListView chatList;
+    ArrayList<String> listOfChats = new ArrayList<String>();
+    ArrayAdapter arrayAdapt;
 
+    String usernameString;
     FirebaseUser firebaseUser;
     DatabaseReference reference;
+    DatabaseReference chatsReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        username = (TextView)findViewById(R.id.username);
+        chatList = (ListView)findViewById(R.id.chatsList);
+
+        arrayAdapt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfChats);
+        chatList.setAdapter(arrayAdapt);
+
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        chatsReference = FirebaseDatabase.getInstance().getReference("Chats");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
-                username = user.getUsername();
+                username.setText("Welcome "+user.getUsername()+"!");
+                usernameString = user.getUsername().toString();
+                Log.d("USERNAME: ", usernameString);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        chatsReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while(i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                arrayAdapt.clear();
+                arrayAdapt.addAll(set);
+                arrayAdapt.notifyDataSetChanged();
             }
 
             @Override
@@ -67,4 +111,5 @@ public class MainActivity extends AppCompatActivity {
 
         return false;
     }
+
 }
